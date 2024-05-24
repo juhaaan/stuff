f68bd899835a80082a84f69b3a1e4c78333e001c
juhaaan <iwantbelieve_kk@yahoo.com>
2024-05-24 23:27:46 +0300

    change cpp file

--- newcfile.cpp
+++ newcfile.cpp
@@ -2,9 +2,6 @@
 
 int main() {
     int i = 0;
-    while (i < 10) {
-        i++;
-        std::cout << "who knows what" << i << "is";
-    }
+    std::cout << "who knows what" << i << "is";
     return 0;
 }
